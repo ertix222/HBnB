1b9dc31dc7eb2f6f9641daf0cd28f6ae9c8e33d8
ertix222 <ertix222@gmail.com>
2025-07-04 10:15:06 +0200

    move import of db to avoid circular import

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from app import db
 from app.models import User, Place, Review, Amenity
 
 class Repository(ABC):
@@ -31,10 +30,13 @@ class Repository(ABC):
 class SQLAlchemyRepository(Repository):
     def __init__(self, model):
         self.model = model
+        from app import db
+        self.db = db
+
 
     def add(self, obj):
-        db.session.add(obj)
-        db.session.commit()
+        self.db.session.add(obj)
+        self.db.session.commit()
 
     def get(self, obj_id):
         return self.model.query.get(obj_id)
@@ -47,13 +49,13 @@ class SQLAlchemyRepository(Repository):
         if obj:
             for key, value in data.items():
                 setattr(obj, key, value)
-            db.session.commit()
+            self.db.session.commit()
 
     def delete(self, obj_id):
         obj = self.get(obj_id)
         if obj:
-            db.session.delete(obj)
-            db.session.commit()
+            self.db.session.delete(obj)
+            self.db.session.commit()
 
     def get_by_attribute(self, attr_name, attr_value):
         return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
