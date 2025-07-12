d03e877d84542339a7f2d68d8f2ee1931b6a319a
Ashe2A <supersonicx001@gmail.com>
2025-07-12 15:11:02 +0200

    HBnB - Auth & DB (5) - Replacing in memory database to sql alchemy

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -17,6 +20,7 @@ def create_app(config_class="config.DevelopmentConfig"):
 
     bcrypt.init_app(app)
     jwt.init_app(app)
+    db.init_app(app)
 
     # api needs to be defined in app before adding namespaces to the swagger
     from app.api.v1.users import api as users_ns
